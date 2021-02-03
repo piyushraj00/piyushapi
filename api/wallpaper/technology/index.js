23c3606d0b19a84a6e6c51b02834727abe5d9e45
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-03 10:05:56 +0700

    Create index.js

---
+++ api/wallpaper/technology/index.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const fs = require('fs').promises
+const fileJson = path.join(__dirname, '..', '..', '..', 'data', 'Technology.json')
+
+module.exports = async (_, res) => {
+    const data = await fs.readFile(fileJson, 'utf-8')
+    const wallpaper = JSON.parse(data)
+    const hasil = {"results" : wallpaper}
+
+    res.status(200).send(hasil)
+}
