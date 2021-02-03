5f2b83caa7c8db3f9958bcb90a74800e81f33a1c
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-03 13:00:42 +0700

    Update index.js

--- api/islamic/niatshubuh/index.js
+++ api/islamic/niatshubuh/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs').promises
-const fileJson = path.join(__dirname, '..', '..', 'data', 'dataNiatShalat.json')
+const fileJson = path.join(__dirname, '..', '..', '..', 'data', 'dataNiatShalat.json')
 
 module.exports = async (_, res) => {
     const data = await fs.readFile(fileJson, 'utf-8')
